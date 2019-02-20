75999de367e6562466830b2205aa2ae3883ad226
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-02-20 22:15:49 +0100

    Start improving the computation of the P-matrix to considerably speed things up.
    Looks like it's almost there, but not quite. Plot 1 and 3 are somewhat resembling the right plots qualitatively while plot 2 is completely
    off.

--- ising_model.py
+++ ising_model.py
@@ -5,7 +5,7 @@ import sympy as sp
 B_ex = 2
 J_par = 1
 J_tan = 1
-n_max = 5 # Really n_max -1
+n_max = 2 # Really n_max -1
 beta_const = 0.5
 
 def gen_spin_conf(n):
@@ -19,7 +19,7 @@ def P_elem(spin_vec1, spin_vec2, Be, beta, Jp, Jt):
 	A = beta*Jp * np.matmul(spin_vec1, spin_vec2)
 	B = beta*Jt * np.matmul(spin_vec1, np.roll(spin_vec1, 1))
 	C = (beta*Be/2) * np.sum((spin_vec1 + spin_vec2))
-	return np.e**(A + B + C)
+	return np.exp(A + B + C)
 
 
 def gen_P_matrix(P_elem, spin_conf, Be, beta, Jp, Jt):
@@ -31,10 +31,59 @@ def gen_P_matrix(P_elem, spin_conf, Be, beta, Jp, Jt):
 
 	return P
 
+# P_test tries to compute A, B and C matrices to compare with the matrices from P_fast
+# P_test and P_fast however returns the same as each other, they are both wrong.
+# Maybe easier to debug P_test, and then apply corresponding corrections to P_fast
+
+def P_test(spin_conf, Be, beta, Jp, Jt):
+	n = spin_conf.shape[1]
+	A = np.zeros([2**n, 2**n])
+	B = np.zeros([2**n, 2**n])
+	C = np.zeros([2**n, 2**n])
+	
+	for i in range(2**n):
+		for j in range(2**n):
+			A[i][j] = beta*Jp * np.matmul(spin_conf[i], spin_conf[j])
+			B[i][j] = beta*Jt * np.matmul(spin_conf[i], np.roll(spin_conf[j], 1))
+			C[i][j] = (beta*Be/2) * np.sum((spin_conf[i] + spin_conf[j]))
+	return A, B, C
+
+
+def P_fast(spin_conf, Be, beta, Jp, Jt):
+	A = beta*Jp * np.matmul(spin_conf, spin_conf.T)
+	B = beta*Jt * np.matmul(spin_conf, np.apply_along_axis(np.roll, 1, spin_conf, 1).T)
+	sum_vec = np.sum(spin_conf, axis=1)
+	C = (beta*Be/2) * np.repeat(sum_vec.reshape(len(sum_vec), 1), len(sum_vec), axis=1) + sum_vec.T
+	return A, B, C
+	#return np.exp(A + B + C)
+
+spin_conf = gen_spin_conf(2)
+A, B, C = P_test(spin_conf, 1, 2, 2, 1)
+A2, B2, C2, = P_fast(spin_conf, 1, 2, 2, 1)
+P1 = gen_P_matrix(P_elem, spin_conf, 1, 1, 1, 1)
+P2 = np.exp(A2 + B2 + C2)
+P3 = np.exp(A + B + C)
+
+print(P1)
+print(" ")
+print(P2)
+print(" ")
+print(P3)
+
+#print(A - A2, "\n\n")
+#print(B - B2, "\n\n")
+#print(C - C2, "\n\n")
+
+'''
 
 def max_eigen_value(beta, P_elem, spin_conf, Be, Jp=J_par, Jt=J_tan):
 	P = gen_P_matrix(P_elem, spin_conf, Be, beta, Jp, Jt)
 	e_vals, e_vecs = np.linalg.eig(P) 
+	return np.amax(np.real(e_vals))
+
+
+def max_eigen_value_fast(beta, P_fast, spin_conf, Be, Jp=J_par, Jt=J_tan):
+	e_vals, e_vecs = np.linalg.eig(P_fast(spin_conf, Be, beta, Jp, Jt))
 	return np.amax(np.real(e_vals))
 
 
@@ -44,8 +93,9 @@ eigen_val_vec = np.zeros([n_max, 100])
 eigen_val_vec_b = np.zeros([n_max, 100])
 eigen_val_vec_d = np.zeros([n_max, 100])
 spes_heat_vec = np.zeros([5, 100])
+'''
 
-
+'''
 for i in range(n_max):
 	spin_conf = gen_spin_conf(i+2)
 	print(i+2)
@@ -60,6 +110,24 @@ for i in range(n_max):
 	print(i+1," done", n_max - i -1, " to go.")
 plt.savefig("fig1.pdf")
 plt.show()
+'''
+'''
+
+for i in range(n_max):
+	spin_conf = gen_spin_conf(i+2)
+	print(i+2)
+	for j in range(beta_vals.size):
+		eigen_val_vec[i][j] = np.log(max_eigen_value_fast(beta_vals[j], P_fast, spin_conf, B_ex))
+		eigen_val_vec_b[i][j] = np.log(max_eigen_value_fast(beta_const, P_fast, spin_conf, B_vals[j]))
+	if i+2 <= 5:
+		for k in range(beta_vals.size):
+			spes_heat_vec[i] = np.gradient(np.gradient(eigen_val_vec[i], beta_vals), beta_vals)*(beta_vals[k]/(i+2))
+	eigen_val_vec_d[i] = np.gradient(eigen_val_vec_b[i], B_vals)/((i+2)*beta_const)
+	plt.plot(beta_vals, eigen_val_vec[i])
+	print(i+1," done", n_max - i -1, " to go.")
+plt.savefig("fig1.pdf")
+plt.show()
+
 
 
 for elem in eigen_val_vec_d:
@@ -72,3 +140,7 @@ for elem in spes_heat_vec:
 	plt.plot(beta_vals, elem)
 plt.savefig("fig3.pdf")
 plt.show()
+
+
+
+'''
